528aaf54c3af2c2d22e5868f6b3e27f437536b4c
valve16 <glushkovoleg2004@yandex.ru>
2024-10-04 23:09:44 +0300

    ch

--- 2048-game/cmd.bat
+++ 2048-game/cmd.bat
@@ -1,10 +1,14 @@
 
 cd 2048-game 
+
 REM Сборка образа 
  docker build -t 2048-game .
 REM Запуск первого контейнера
- docker run -d -p 8080:80 --name 2048-game 2048-game
+
+ docker run -d -p 8080:80 --name game1 2048-game
 REM Запуск второго контейнера
- docker run -d -p 8081:80 --name 2048-game-2 2048-game
+
+ docker run -d -p 8081:80 --name game2 2048-game
+
  docker ps
 pause
